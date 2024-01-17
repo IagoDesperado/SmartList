842a6bce0ce32872942ee26d3cd7654af42af86d
IagoDesperado <126955467+IagoDesperado@users.noreply.github.com>
2024-01-17 12:30:29 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,229 +1,43 @@
-import 'dart:ffi';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:provider/provider.dart';
+import 'package:logging/logging.dart';
 
-void main() {
-  runApp(MyApp());
-}
+final log = Logger('MainAppLogger');
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  var favorites = <WordPair> [];
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  bool isFavorite() {
-    if(favorites.contains(current)) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
-  void toggleFavorite () {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    }
-    else {
-      favorites.add(current);
-    }
-
-    notifyListeners();
-  }
-
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
- 
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-
-  Widget page;
-    switch (selectedIndex) {
-    case 0:
-      page = GeneratorPage();
-      break;
-    case 1:
-      page = FavouritesPage();
-      break;
-    case 2:
-      page = LoginPage();
-      break;
-    default:
-      throw UnimplementedError('no widget for $selectedIndex');
-}
-
-    return LayoutBuilder(
-      builder: (context,Constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              
-              SafeArea(
-                child: NavigationRail(
-                  extended: false,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.person), 
-                      label: Text('Test') )
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
-    );
-  }
-}
-
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.isFavorite()) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FavouritesPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    var appState = context.watch<MyAppState>();
-    
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-
-    return ListView(
-      children: [
-        Card(
-          color: Colors.blueGrey,
-          child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('Your favorites:'),
-        )
-        ),
-        for (var favorite in appState.favorites)
-          ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text(favorite.asLowerCase),
-            ),
-        ],
-    );
-    }
-  }
-
-class LoginPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    var appState = context.watch<MyAppState>();
+    const appTitle = 'SmartList';
 
     return MaterialApp(
-      title: 'Login Screen',
-      theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 104, 149, 255),
+            foregroundColor: Color.fromARGB(255, 253, 135, 255)//here you can give the text color
+            ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Color.fromARGB(255, 253, 135, 255),//here you can give the text color,
+            backgroundColor: Color.fromARGB(255, 104, 149, 255),
+          ),
         ),
+        //primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 253, 135, 255)),//here you can give the text color
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Login Screen'),
+          title: const Text(appTitle),
         ),
+        backgroundColor:  Color.fromARGB(255, 104, 149, 255),
         body: const MyCustomForm(),
       ),
     );
-
   }
 }
 
@@ -234,6 +48,20 @@ class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
     return MyCustomFormState();
+  }
+}
+
+//widget that contains the logo
+class LogoState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Image.asset(
+        'assets/logo.png', // path to the logo
+        height: 100,
+      ),
+    );
   }
 }
 
@@ -253,68 +81,314 @@ class MyCustomFormState extends State<MyCustomForm> {
     return Form(
       key: _formKey,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextFormField(
+          LogoState(), //display the logo
+          //put the taxt bars in a container to limit their width
+          Center(
+            child:Container(
+                width: 500, // Set the desired width
+                  child: TextFormField(
+                  //styling of the textbvars
+                  style: TextStyle(color: Color.fromARGB(255, 68, 0, 255)), // Set text color
+                  decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white, // Set background color
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
             // The validator receives the text that the user has entered.
             validator: (value) {
               if (value == null || value.isEmpty) {
-                return 'Please enter username';
+                return 'Please enter a valid username';
               }
               return null;
             },
           ),
-          TextFormField(
+          ),
+      ),
+          Container(
+            width: 500,  
+            child:TextFormField(
+              style: TextStyle(color: Color.fromARGB(255, 68, 0, 255)), // Set text color
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white, // Set background color
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ), 
             // The validator receives the text that the user has entered.
             validator: (value) {
               if (value == null || value.isEmpty) {
-                return 'Please enter password';
+                return 'Please enter a password';
               }
               return null;
             },
+          ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
+            padding: const EdgeInsets.symmetric(vertical: 32),
             child: ElevatedButton(
               onPressed: () {
                 // Validate returns true if the form is valid, or false otherwise.
+                //TODO: actually implement a database and save the data
                 if (_formKey.currentState!.validate()) {
                   // If the form is valid, display a snackbar. In the real world,
                   // you'd often call a server or save the information in a database.
                   ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
+                    const SnackBar(content: Text("The db hasn't been implemented yet so this is pretty meaningless.Just like life.")),
                   );
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
                 }
               },
-              child: const Text('Submit'),
+              child: const Text('Log In'),
             ),
           ),
+          Text('Forgot your password?')
         ],
       ),
     );
   }
 }
 
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
 
-  final WordPair pair;
+  static const appTitle = 'SmartList';
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+      theme: ThemeData(
+         appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 104, 149, 255),
+            foregroundColor: Color.fromARGB(255, 253, 135, 255)//here you can give the text color
+            ),
+        scaffoldBackgroundColor: Color.fromARGB(255, 104, 149, 255), // Change this color to your desired background color
+    ),
     );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style, semanticsLabel: "${pair.first} ${pair.second}"),
+  }
+}
+
+class Product {
+  const Product({required this.name});
+
+  final String name;
+}
+
+typedef CartChangedCallback = Function(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({
+    required this.product,
+    required this.inCart,
+    required this.onCartChanged,
+  }) : super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  Color _getColor(BuildContext context) {
+    // The theme depends on the BuildContext because different
+    // parts of the tree can have different themes.
+    // The BuildContext indicates where the build is
+    // taking place and therefore which theme to use.
+
+    return inCart //
+        ? Colors.black54
+        : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
       ),
+      title: Text(
+        product.name,
+        style: _getTextStyle(context),
+      ),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, super.key});
+
+  final List<Product> products;
+
+  // The framework calls createState the first time
+  // a widget appears at a given location in the tree.
+  // If the parent rebuilds and uses the same type of
+  // widget (with the same key), the framework re-uses
+  // the State object instead of creating a new State object.
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      // When a user changes what's in the cart, you need
+      // to change _shoppingCart inside a setState call to
+      // trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        children: widget.products.map((product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  
+  static Widget _buildPage(int index) {
+    switch (index) {
+      case 0:
+        return ShoppingList(
+        products: [
+          Product(name: 'Product 1'),
+          Product(name: 'Product 2'),
+          ]
+        );
+      case 1:
+        return Text(
+          'Index 1: Business',
+          style: optionStyle,
+        );
+      case 2:
+        return Text(
+          'Index 2: School',
+          style: optionStyle,
+        );
+      default:
+        return Container(); // You can return an empty container or handle the case as needed
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: _buildPage(_selectedIndex),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Color.fromARGB(255, 104, 149, 255),
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+          child: ListView(
+          // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                color: Color.fromARGB(255, 253, 135, 255),
+              ),
+              child: Text('Settings'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Business'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('School'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(2);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    ),
+      backgroundColor: Color.fromARGB(255, 104, 149, 255),
     );
   }
 }
